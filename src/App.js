a2b1ab775ac6bd9835881b1f13d6a6c29ff73e14
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-12-12 22:07:02 -0500

    End of pomodoro commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <p>Home</p>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <p>Home</p>
+      </div>
+    )
+  }
 }
 
 export default App;
