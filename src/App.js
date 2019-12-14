0100bfa06ea9c9944236e31b9e8a56ab85786e26
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-12-13 20:52:14 -0500

    End of pomodoro commit
    Need to refactor App component to use routes instead of conditional
    rendering

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from 'react-router-dom';
 //Components
 import Login from'./components/Login/Login';
 import Home from './components/Home/Home';
+import Signup from './components/Signup/Signup';
 
 class App extends Component {
 
@@ -19,6 +20,12 @@ class App extends Component {
   }
 
   render () {
+    //Need to refactor to use routes instead of conditional rendering
+    <div>
+    {/*Routes*/}
+    <Route path='/signup' component={Signup} />
+    </div>
+
     if (!this.state.loginedIn) {
       return <Login 
         handleLogin={this.handleLogin}
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 //components
 import Signup from '../Signup/Signup';
 
 const Login = (props) => {
 	return (
 		<div>
-			{/*Routes*/}
-			<Route path='/signup' component={Signup} />
-
 			<h1>Login Page</h1>
 			<button onClick={props.handleLogin}>Login</button>
 			<button><Link to='/signup'>Signup</Link></button>
